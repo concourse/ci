37bd0a673fdd4be7abbd50e2fb525bd54d8cbd63
Alex Suraci <suraci.alex@gmail.com>
2019-12-17 11:17:09 -0500

    allow smoke tests to run without HTTPS
    the still-supported 5.2.x version line doesn't have Let's Encrypt
    support, so we need to make this conditional
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- deployments/smoke/smoke.tf
+++ deployments/smoke/smoke.tf
@@ -12,6 +12,11 @@ variable "region" {
   default = "us-central1"
 }
 
+variable "use_https" {
+  type    = bool
+  default = true
+}
+
 provider "google" {
   credentials = "keys/gcp.json"
   project     = var.project
@@ -109,7 +114,7 @@ data "template_file" "web_conf" {
   template = file("systemd/smoke-web.conf.tpl")
 
   vars = {
-    instance_url   = "https://${google_compute_address.smoke.address}.xip.io"
+    instance_ip    = google_compute_address.smoke.address
     admin_password = random_string.admin_password.result
     guest_password = random_string.guest_password.result
   }
@@ -189,7 +194,7 @@ resource "null_resource" "rerun" {
 }
 
 output "instance_url" {
-  value = "https://${google_compute_address.smoke.address}.xip.io"
+  value = var.use_https ? "https://${google_compute_address.smoke.address}.xip.io" : "http://${google_compute_address.smoke.address}:8080"
 }
 
 output "admin_password" {
