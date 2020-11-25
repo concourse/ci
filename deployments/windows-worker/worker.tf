99d8d50fe424420f99f99d147ba3ec21048cc6c2
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-11-25 13:08:09 -0500

    bump windows boot disk to 64GB
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- deployments/windows-worker/worker.tf
+++ deployments/windows-worker/worker.tf
@@ -46,7 +46,7 @@ resource "google_compute_instance" "windows_worker" {
   boot_disk {
     initialize_params {
       image = "windows-server-2004-dc-core-v20201110"
-      size  = "32"
+      size  = "64"
     }
   }
 
