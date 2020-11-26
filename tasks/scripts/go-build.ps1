06e4245476df7041014636148012930bd57e795e
Alex Suraci <suraci.alex@gmail.com>
2020-11-26 18:58:23 -0500

    explicitly add git/mingw to $env:Path
    honestly, I can't tell why Chocolatey's shims or $env:Path updating
    isn't working; it works when I RDP in to make a new shell, but doesn't
    work when I 'refreshenv' in a 'fly intercept'.
    
    note: previous version of this also added Go to the $env:Path, but that
    part seems to be working fine, so let's just add git and mingw.
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- tasks/scripts/go-build.ps1
+++ tasks/scripts/go-build.ps1
@@ -4,5 +4,7 @@ trap { $host.SetShouldExit(1) }
 # This is where go puts intermediate build artifacts, esp during testing
 $env:TMP = "$pwd\tmp"
 
+$env:Path += ";C:\Program Files\Git\cmd;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin"
+
 $env:GOPATH = "$pwd\gopath"
 $env:Path += ";$pwd\gopath\bin"
