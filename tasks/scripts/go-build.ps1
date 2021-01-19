1381af9a7260f55083463747fc346412a1e82055
Alex Suraci <suraci.alex@gmail.com>
2021-01-19 14:42:13 -0500

    windows: create tmp directory
    ...now that this isn't being created for us, because the task cache was
    removed
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- tasks/scripts/go-build.ps1
+++ tasks/scripts/go-build.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = "Stop"
 trap { $host.SetShouldExit(1) }
 
 # This is where go puts intermediate build artifacts, esp during testing
+mkdir "$pwd\tmp"
 $env:TMP = "$pwd\tmp"
 
 $env:GOPATH = "$pwd\gopath"
