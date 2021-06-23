8d7b71777fdd1a984f67afd85bc382ff96f0e23d
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-06-23 16:12:57 -0400

    provide service account key for k8s-topgun auth
    previously, the kube_config we provided to tasks that tested against the
    k8s topgun cluster used basic auth to authenticate with the cluster. GKE
    deprecated this, however (see concourse/infrastructure#12).
    
    instead, the approach we're taking is to provide a kube_config to each
    task that has no credentials, but instructs kubernetes to authenticate
    using a GCP service account. we also provide the service account key and
    write it to disk, and set the GOOGLE_APPLICATION_CREDENTIALS env var so
    kubectl can locate it.
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>
    Co-authored-by: Esteban Foronda <eforonda@vmware.com>

---
+++ tasks/scripts/k8s-helpers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ensure_kube_config() {
+  mkdir -p ~/.kube
+
+  if [[ -f ~/.kube/config ]]; then
+    return 0
+  fi
+
+  if [[ -z $KUBE_CONFIG ]]; then
+    echo "Error: KUBE_CONFIG must be specified when ~/.kube/config doesnt exist"
+    exit 1
+  fi
+
+  echo "$KUBE_CONFIG" >~/.kube/config
+
+  # Our kube config uses the gcp auth-provider to fetch an access token
+  # dynamically using a service account, so this must be set.
+  echo "$SERVICE_ACCOUNT_KEY" > ~/service-account.json
+  export GOOGLE_APPLICATION_CREDENTIALS=~/service-account.json
+}
