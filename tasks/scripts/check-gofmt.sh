df6f798e1ff6c2a0538bfac76f7d06b21df47236
Taylor Silva <tasilva@vmware.com>
2021-10-18 13:38:33 -0400

    Add helpful messages for go format check
    Signed-off-by: Taylor Silva <tasilva@vmware.com>

--- tasks/scripts/check-gofmt.sh
+++ tasks/scripts/check-gofmt.sh
@@ -6,8 +6,10 @@ result=$(goimports -l ./**/*.go)
 number_of_changed_files=$(echo ${result} | wc -l)
 
 if [[ ${number_of_changed_files} -ne 0 ]]; then
+  echo "!!! Please format the following files using golang.org/x/tools/cmd/goimports !!!\n"
+  echo "Install goimports with: go install golang.org/x/tools/cmd/goimports@latest\n"
   echo ${result}
   exit 1
 fi
 
-echo "formatting looks good!"
+echo "Formatting looks good!"
