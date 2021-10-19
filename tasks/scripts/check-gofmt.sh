09b5b176fccfdff455f2ca7e088c2cd35bf53856
Taylor Silva <tasilva@vmware.com>
2021-10-19 09:55:00 -0400

    echo new lines threw everything off by one!
    echo -n will not include a new line. echo includes a newline by default
    
    Signed-off-by: Taylor Silva <tasilva@vmware.com>

--- tasks/scripts/check-gofmt.sh
+++ tasks/scripts/check-gofmt.sh
@@ -3,11 +3,11 @@
 cd concourse
 
 result=$(goimports -l ./**/*.go)
-number_of_changed_files=$(echo ${result} | wc -l)
+number_of_changed_files=$(echo -n ${result} | wc -l)
 
 if [[ ${number_of_changed_files} -ne 0 ]]; then
-  echo "!!! Please format the following files using golang.org/x/tools/cmd/goimports !!!\n"
-  echo "Install goimports with: go install golang.org/x/tools/cmd/goimports@latest\n"
+  echo "!!! Please format the following files using golang.org/x/tools/cmd/goimports !!!"
+  echo "Install goimports with: go install golang.org/x/tools/cmd/goimports@latest"
   echo ${result}
   exit 1
 fi
