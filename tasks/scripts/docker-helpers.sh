66466b8a9fd0f2b315711d4408c3f559473676bd
Taylor Silva <dev@taydev.net>
2024-09-22 12:25:40 -0400

    update with-docker-compose task to work with cgroupv2 workers
    we have to use the containerd runtime now since it's the only runtime
    that works with cgroups v2. Thankfully this actually makes running DIND
    easier for us. We don't have to setup cgroups anymore. /sys/fs/cgroups
    comes read+write mounted already
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- tasks/scripts/docker-helpers.sh
+++ tasks/scripts/docker-helpers.sh
@@ -2,18 +2,9 @@
 
 set -euo pipefail
 
-source ci/tasks/scripts/cgroup-helpers.sh
-
 function start_docker() {
   mkdir -p /var/log
   mkdir -p /var/run
-
-  sanitize_cgroups
-
-  # check for /proc/sys being mounted readonly, as systemd does
-  if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
-    mount -o remount,rw /proc/sys
-  fi
 
   local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
   local server_args="--mtu ${mtu}"
