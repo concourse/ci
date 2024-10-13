fa52c7495b9568c848c4e9538924f2f57c62e608
Taylor Silva <dev@taydev.net>
2024-10-13 17:29:07 -0400

    add note about why sc changes are necessary
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- tasks/k8s-topgun/deploy.sh
+++ tasks/k8s-topgun/deploy.sh
@@ -32,13 +32,19 @@ mkdir -p "${HOME}/.kube"
 cp "${output}/config" "${HOME}/.kube/config"
 
 echo "changing default storage class"
+# For some reason the default stoarge class that linode sets results in
+# volumes being made but never attached to the nodes. Switching to the
+# non-default storage class fixes this for whatever reason. This is fine
+# anyways as we don't want volumes that we retain, we want them destroyed
+# after we're done using them which is what the non-default storage class
+# does
 kubectl patch storageclass linode-block-storage-retain -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
 kubectl patch storageclass linode-block-storage -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 kubectl get storageclass
 
 echo "Waiting for all nodes in node pool to be ready"
 while true; do
-  pods="$(kubectl get nodes --no-headers | wc -l)"
+  pods="$(kubectl get nodes --no-headers | grep -v NotReady | wc -l)"
   # the ${// /} is to remove any spaces
   if [[ "${pods// /}" == "8" ]]; then
     echo "All pods connected to the control plane"
