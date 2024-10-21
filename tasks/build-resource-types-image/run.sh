82afd8ee2829c0c09d6bfa741820a4ef50c4e751
Taylor Silva <dev@taydev.net>
2024-10-20 21:18:58 -0400

    fetch busybox base image for resource types
    Signed-off-by: Taylor Silva <dev@taydev.net>

---
+++ tasks/build-resource-types-image/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+for resource in $(ls | grep resource); do
+    echo repacking ${resource}
+    cd ${resource}
+        resource=$(echo ${resource%-resource})
+        tar czf rootfs.tgz --directory=rootfs/ .
+        rm -rf ./rootfs/
+
+        vr=$(cat tag)
+        version=$(echo ${vr%%-*})
+        privileged=$(if [[ ${resource} == "docker-image" ]]; then echo true; else echo false; fi;)
+        version_history=$(if [[ ${resource} == "time" ]]; then echo true; else echo false; fi;)
+
+        echo { >> resource_metadata.json
+        echo \ \ \"type\": \"${resource}\", >> resource_metadata.json
+        echo \ \ \"version\": \"${version}\", >> resource_metadata.json
+        echo \ \ \"privileged\": ${privileged}, >> resource_metadata.json
+        echo \ \ \"unique_version_history\": ${version_history} >> resource_metadata.json
+        echo } >> resource_metadata.json
+
+        rm metadata.json repository digest tag
+    cd -
+done;
+
+build
