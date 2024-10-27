b94fa6cb1b169cc74fa640a36e789766d15e7ecd
Taylor Silva <dev@taydev.net>
2024-10-27 16:02:13 -0400

    remove bosh-upload-release job
    I'm not 100% sure but I think this was only needed for prepping the bosh
    testing jobs
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- tasks/bosh-upload-releases/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-#TODO: use minio to manually upload to blobstore without director
-bosh upload-release concourse-release/*.tgz
-bosh upload-release postgres-release/*.tgz
-bosh upload-release bpm-release/*.tgz
