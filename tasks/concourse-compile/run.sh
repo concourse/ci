1386c816e3d0d65477ccb8663e9480bfa45110d2
Taylor Silva <dev@taydev.net>
2025-03-07 12:59:00 -0500

    add compile-check job to PRs pipeline
    This job compiles the main concourse binary for all platforms. This is
    to catch any obvious compile errors that may have arisen from
    linux-specific changes.
    
    This commit adds a task the only compiles the concoures binary since
    that's all we need to do to satisfy this check.
    
    Also added on_abort and on_error steps for the pr notification since we
    do have timeouts on the jobs and it would be nice to notify the PR that
    the checked is no longer running.
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

---
+++ tasks/concourse-compile/run.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export GOPATH=$PWD/gopath
+export PATH=$PWD/gopath/bin:$PATH
+
+if [ -z "${PLATFORM}" ]; then
+  echo "usage: PLATFORM=<platform> $0" >&2
+  exit 1
+fi
+
+export GOOS="${PLATFORM:-$(go env GOOS)}"
+export GOARCH="${ARCH:-amd64}"
+
+final_version=""
+ldflags=""
+if [ -e final-version/version ]; then
+  final_version="$(cat final-version/version)"
+  ldflags="-X github.com/concourse/concourse.Version=$final_version"
+fi
+
+pushd concourse
+  bin_name="concourse"
+  extra_flags=""
+  if [ "$GOOS" = "windows" ]; then
+    bin_name+=".exe"
+  fi
+
+  if [ "$GOOS" = "darwin" ]; then
+    extra_flags="-buildvcs=false"
+  fi
+
+  go build -o "$bin_name" -ldflags "$ldflags" $extra_flags ./cmd/concourse
+
+  if [ -n "$final_version" ]; then
+    if [ "$GOOS" = "linux" ]; then
+      test "$(./concourse --version)" = "$final_version"
+    else
+      echo "skipping --version check for platform $GOOS"
+    fi
+  fi
+popd
+
+mv concourse/$bin_name concourse-binary/
