30b5cc0235a669a785f9c898a454c7b05cc604cd
Taylor Silva <dev@taydev.net>
2025-06-06 16:56:22 -0400

    update validate-linux-rc task to test amd64 and arm64
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- tasks/validate-linux-rc/run.sh
+++ tasks/validate-linux-rc/run.sh
@@ -1,48 +1,56 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -euo
+set -euo pipefail
 
 # install ldd
 apk add --quiet --no-progress posix-libc-utils
 
-tar -xzf concourse-tarball/concourse-*.tgz
-cd concourse
+for arch in amd64 arm64; do
+    tar -xzf concourse-tarballs/concourse-*linux.${arch}.tgz
+    pushd concourse
 
-# ensure all resource types are bundled
-resource_types='bosh-io-release
-bosh-io-stemcell
-docker-image
-git
-github-release
-hg
-mock
-pool
-registry-image
-s3
-semver
-time'
+    # ensure all resource types are bundled
+    resource_types=(
+        "bosh-io-release"
+        "bosh-io-stemcell"
+        "docker-image"
+        "git"
+        "github-release"
+        "hg"
+        "mock"
+        "pool"
+        "registry-image"
+        "s3"
+        "semver"
+        "time"
+    )
 
-for resource_type in $resource_types; do
-  cat resource-types/$resource_type/resource_metadata.json
-  test -f resource-types/$resource_type/rootfs.tgz
+    for resource_type in "${resource_types[@]}"; do
+        cat "resource-types/$resource_type/resource_metadata.json"
+        test -f "resource-types/$resource_type/rootfs.tgz"
+    done
+
+    if [[ $arch == "amd64" ]]; then
+        # test that binary is statically linked
+        if ldd bin/concourse; then
+            echo "concourse binary is not static; aborting"
+            exit 1
+        fi
+        ./bin/concourse --version
+
+        tar -xzf fly-assets/fly-linux-amd64.tgz
+        if ldd fly; then
+            echo "fly binary is not static; aborting"
+            exit 1
+        fi
+        ./fly --version
+    fi
+
+    # sanity check for concourse dependencies
+    test -f bin/containerd
+    test -f bin/runc
+    test -f bin/gdn
+    test -f bin/init
+
+    popd
 done
-
-# test that binaries are statically linked
-if ldd bin/concourse; then
-  echo "concourse binary is not static; aborting"
-  exit 1
-fi
-./bin/concourse --version
-
-tar -xzf fly-assets/fly-linux-amd64.tgz
-if ldd fly; then
-  echo "fly binary is not static; aborting"
-  exit 1
-fi
-./fly --version
-
-# sanity check for concourse dependencies
-test -f bin/containerd
-test -f bin/runc
-test -f bin/gdn
-test -f bin/init
