13ec74ab1f61855f41ab7dbc7738320875829dbc
Taylor Silva <dev@taydev.net>
2025-07-03 13:59:04 -0400

    update prep release to work for multi-arch release
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- tasks/prep-release-assets/run.sh
+++ tasks/prep-release-assets/run.sh
@@ -2,24 +2,29 @@
 
 set -euo pipefail -x
 
+apk add --quiet --no-progress cmd:shasum
+
 version=$(cat version/version)
 
-mv linux-rc/*.tgz   "concourse-linux/concourse-${version}-linux-amd64.tgz"
-mv windows-rc/*.zip "concourse-windows/concourse-${version}-windows-amd64.zip"
-mv darwin-rc/*.tgz  "concourse-darwin/concourse-${version}-darwin-amd64.tgz"
+mv linux-amd64-rc/*.tgz   "concourse-linux/concourse-${version}-linux-amd64.tgz"
+mv linux-arm64-rc/*.tgz   "concourse-linux/concourse-${version}-linux-arm64.tgz"
+mv windows-amd64-rc/*.zip "concourse-windows/concourse-${version}-windows-amd64.zip"
+mv darwin-amd64-rc/*.tgz  "concourse-darwin/concourse-${version}-darwin-amd64.tgz"
+mv darwin-arm64-rc/*.tgz  "concourse-darwin/concourse-${version}-darwin-arm64.tgz"
 
-tar -zxf concourse-linux/*.tgz concourse/fly-assets --strip-components=2
+tar -zxf "concourse-linux/concourse-${version}-linux-amd64.tgz" concourse/fly-assets --strip-components=2
 
-mv fly-linux-*.tgz   "fly-linux/fly-${version}-linux-amd64.tgz"
+mv fly-linux-amd64.tgz   "fly-linux/fly-${version}-linux-amd64.tgz"
+mv fly-linux-arm64.tgz   "fly-linux/fly-${version}-linux-arm64.tgz"
 mv fly-windows-*.zip "fly-windows/fly-${version}-windows-amd64.zip"
-mv fly-darwin-*.tgz  "fly-darwin/fly-${version}-darwin-amd64.tgz"
+mv fly-darwin-amd64.tgz  "fly-darwin/fly-${version}-darwin-amd64.tgz"
+mv fly-darwin-arm64.tgz  "fly-darwin/fly-${version}-darwin-arm64.tgz"
 
-for asset in {concourse,fly}-{linux,windows,darwin}/*; do
-  dir=$(dirname "$asset")
-  file=$(basename "$asset")
-
-  # ensure .sha1 file just has the filename so shasum -c works
+for dir in {concourse,fly}-{linux,windows,darwin}; do
   pushd "$dir"
-    shasum "$file" > "${file}.sha1"
+    for file in *; do
+        # ensure .sha1 file just has the filename so shasum -c works
+        shasum "$file" > "${file}.sha1"
+    done
   popd
 done
