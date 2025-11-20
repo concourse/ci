1bae71d94c4271963c5e180caf175be76402072e
Taylor Silva <dev@taydev.net>
2025-11-20 11:40:23 -0500

    latest helm version does some kind of check on plugins now
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- tasks/helm-chart-unit-tests/run.sh
+++ tasks/helm-chart-unit-tests/run.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail -x
 
-helm plugin install https://github.com/helm-unittest/helm-unittest.git
+helm plugin install --verify=false https://github.com/helm-unittest/helm-unittest.git
 
 cd chart
 helm unittest -f test/unittest/**/*.yaml .
