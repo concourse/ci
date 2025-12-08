da1d1fef1a9c91bdbdff0ee5c67ffb962d2873a6
Taylor Silva <dev@taydev.net>
2025-12-08 14:35:31 -0500

    get latest tag across all branches
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- tasks/get-dev-version/run.sh
+++ tasks/get-dev-version/run.sh
@@ -6,7 +6,7 @@ apk --no-cache --no-progress add git
 
 current_ref="$(git -C concourse rev-parse --short HEAD)"
 
-latest_tag="$(git -C concourse describe --tags --abbrev=0 HEAD)"
+latest_tag="$(git tag --sort=-v:refname | head -n 1)"
 timestamp="$(date +%s)"
 
 echo "${latest_tag//v/}+dev.${timestamp}.${current_ref}" > version/version
