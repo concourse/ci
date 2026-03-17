ed63e69e64ab0b69cdb481977b6f698583bf7dcd
Taylor Silva <dev@taydev.net>
2026-03-16 20:29:50 -0400

    build containerd binaries and rename runc pipeline
    Signed-off-by: Taylor Silva <dev@taydev.net>

---
+++ tasks/containerd/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+apk --no-progress add git make
+
+cd containerd
+git checkout "$CONTAINERD_TAG"
+
+make STATIC=1 GOARCH=amd64
+
+# Log versions
+./bin/containerd --version
+./bin/containerd-stress --version
+./bin/containerd-shim-runc-v2 --version
+./bin/ctr --version
+
+cd ./bin
+tar czvf containerd.amd64.tar.gz ./
+mv containerd.amd64.tar.gz ../
+cd ..
+
+rm -rf ./bin
+
+make STATIC=1 GOARCH=arm64
+cd ./bin
+tar czvf containerd.arm64.tar.gz ./
+mv containerd.arm64.tar.gz ../
+cd ..
+
+mv ./containerd.*.tar.gz ../containerd-tar/
