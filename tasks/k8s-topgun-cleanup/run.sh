f95f31880f44214ddcc2283fb26ad144d75ae76f
Taylor Silva <dev@taydev.net>
2026-06-16 13:56:41 -0400

    convert to busybox date format
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- tasks/k8s-topgun-cleanup/run.sh
+++ tasks/k8s-topgun-cleanup/run.sh
@@ -12,5 +12,5 @@ source "ci/tasks/scripts/k8s-helpers.sh"
 gke_auth
 
 echo "Deleting any topgun-* namespaces older than 4hrs"
-cutoff=$(date -d '4 hours ago' +%s)
+cutoff=$(date -d @$(( $(date +%s) - 4*3600 )) +%s)
 kubectl get ns -o jsonpath='{range .items[*]}{.metadata.name}{" "}{.metadata.creationTimestamp}{"\n"}{end}' | while read -r name ts; do [[ "$name" == topgun-* ]] && [ "$(date -d "$ts" +%s)" -lt "$cutoff" ] && kubectl delete ns "$name"; done
